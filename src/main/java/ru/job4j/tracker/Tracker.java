be6cbf5f8d875cb524d93a60efbe8f65d8b01cb6
Administrator <kh-a-r@yandex.ru>
2021-01-13 00:51:14 +0500

    5. Tracker - хранилище [#426067]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -28,34 +28,58 @@ public class Tracker {
 
     public Item[] findAll() {
         Item[] itemsWithoutNull = new Item[items.length];
-        int size = 0;
+        int count = 0;
         for (int i = 0; i < items.length; i++) {
             if (items[i] != null) {
-                itemsWithoutNull[size] = items[i];
-                size++;
+                itemsWithoutNull[count] = items[i];
+                count++;
             }
         }
-        return itemsWithoutNull = Arrays.copyOf(itemsWithoutNull, size);
+        return Arrays.copyOf(itemsWithoutNull, count);
     }
 
     public Item[] findByName(String key) {
         Item[] eqName = new Item[items.length];
         int size = 0;
         for (int i = 0; i < items.length; i++) {
-            if (items[i].getName().equals(key)) {
+            if ( items[i].getName().equals(key)) {
                 eqName[size] = items[i];
                 size++;
             }
         }
-        return eqName = Arrays.copyOf(items, size);
+        return eqName = Arrays.copyOf(eqName, size);
+    }
+
+    public static void main(String[] args) {
+        Item item = new Item();
+        item.setName("Item");
+        Item item1 = new Item();
+        // item1.setName("Item1");
+        Item item2 = new Item();
+        item2.setName("Item2");
+        Tracker tracker = new Tracker();
+       /* LocalDateTime date = item.getCreated();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        String currentDateTimeForm = date.format(formatter);
+        System.out.println(currentDateTimeForm); */
+
+        Item rsl = tracker.add(item);
+        Item rsl1 = tracker.add(item1);
+        Item rsl2 = tracker.add(item2);
+
+        Item[] noNullArray = tracker.findAll();
+        for (int i = 0; i < noNullArray.length; i++) {
+            System.out.print(noNullArray[i].getName() + " ");
+        }
+        System.out.println();
+        Item[] addArray = tracker.items;
+        for (int i = 0; i < addArray.length; i++) {
+            System.out.print(addArray[i].getName() + " ");
+        }
+
     }
 
 
-    public static void main(String[] args) {
-    Item[] items = new Item[5];
-    Item item2 = new Item("Item2");
-    Tracker tracker = new Tracker();
-    System.out.println(tracker.add(item2).getId() + " " + tracker.add(item2).getName());
-        }
+
     }
 
