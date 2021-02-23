92fe0c184f21cdc7fd7e4d43d7181f8d056312c0
Administrator <kh-a-r@yandex.ru>
2021-02-23 13:12:31 +0500

    4. final [#426027]

---
+++ src/main/java/ru/job4j/ooa/Airbus.java
@@ -0,0 +1,43 @@
+package ru.job4j.ooa;
+
+public final class Airbus extends Aircraft{
+    private static final int COUNT_ENGINE = 2;
+
+    private String name;
+
+    public Airbus(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void printModel() {
+        System.out.println("Модель самолета: " + name);
+    }
+
+    public void printCountEngine() {
+        if (name.equals("A380")) {
+            System.out.println("Количество двигателей равно: " + (COUNT_ENGINE + 2));
+        } else {
+            System.out.println("Количество двигателей равно: " + COUNT_ENGINE);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Airbus{"
+                + "name='" + name + '\''
+                + '}';
+    }
+}
+//метод printCountEngine() должен выводить реальное количество двигателей,
+// которое присуще той или иной модели самолета.
+// Поэтому если модель самолета A380 - количество двигателей должно равняться 4,
+// для всех остальных моделей - используем в качестве значения константу COUNT_ENGINE.
